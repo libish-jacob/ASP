89feffd5240d8fdb49ad7202348b4b4eab495cb2
Libish Jacob <jajac@vestas.com>
2018-05-21 17:11:37 +0530

    description added
    description added

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -29,9 +29,15 @@ namespace WebApplication.Controllers
             return View();
         }
 
-        [Route("home/hello/{year}/{month}")]
+        [Route("home/{year}/{month}")]
         public ActionResult Hello(int? year, int? month)
         {
+            /*
+             * Here request is routed to this method based on the pattern. View is selected based on the method name.
+             * We dont use View bag as it is dynamic and no compile time safety. We dont use ViewData as it depends on magic string and can complicate maintenance as we have to modify both controller and view if the string has to be changed.
+             * We only use the model approach.
+             */
+
             Movie newMovie = new Movie { Name = "Movie1" };
             IList<Customer> cust = new List<Customer> {
                 new Customer{ Name = "1" },
@@ -39,7 +45,7 @@ namespace WebApplication.Controllers
             };
 
             HomeViewModel viewModel = new HomeViewModel() { movie = newMovie, Customers = cust };
-            return View(viewModel);            
+            return View(viewModel);                        
         }
     }
 }
